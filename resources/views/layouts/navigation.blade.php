47f6e1c579da777aed7126f68f5bce8d8d2d19e2
Paul Rezzonico Ubuntu <paul.rezzo@ubuntu.com>
2023-10-15 15:45:27 +0200

    feat(navbar) upload navbar categories

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,14 +24,13 @@
         </div>
 
         <div class="flex flex-1 md:w-1/3 justify-center md:justify-start text-white px-2 ">
-            <span class="relative w-full">
-                <input aria-label="search" type="search" id="search" placeholder="Search" class="w-full bg-gray-900 text-white transition border border-transparent focus:outline-none focus:border-gray-400 rounded py-3 px-2 pl-10 appearance-none leading-normal">
-                <div class="absolute search-icon" style="top: 1rem; left: .8rem;">
-                    <svg class="fill-current pointer-events-none text-white w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
-                    </svg>
-                </div>
-            </span>
+            <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
+                <li><a href="/produits" class="text-gray-400 hover:text-gray-200 py-2 px-4">Produits</a></li>
+                @if(auth()->check())
+                <li><a href="/mes-produits" class="text-gray-400 hover:text-gray-200 py-2 px-4">Mes produits</a></li>
+                <li><a href="/ma-wishlist" class="text-gray-400 hover:text-gray-200 py-2 px-4">Ma wishlist</a></li>
+                @endif
+            </ul>
         </div>
 
         <div class="flex w-full pt-2 content-center justify-between md:w-1/3 md:justify-end">
