f5ca00cb2e900357c444c2de864cd1a6e879bc71
Paul Rezzonico Ubuntu <paul.rezzo@ubuntu.com>
2023-10-15 20:29:32 +0200

    feat(front) ad better front for mail and confirm message

--- resources/views/confirmation.blade.php
+++ resources/views/confirmation.blade.php
@@ -6,11 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confirmation</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+            background: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+        }
+    </style>
 </head>
-<body class="bg-gray-100 h-screen flex justify-center items-center">
-    <div class="bg-white p-8 rounded-lg shadow-md w-1/3">
-        <h1 class="text-2xl font-bold mb-4">Confirmation</h1>
-        <p class="text-gray-700">L'email a été envoyé avec succès. Vous serez redirigé dans quelques instants...</p>
+<body class="h-screen flex justify-center items-center">
+    <div class="bg-white p-8 rounded-lg shadow-xl w-1/3 space-y-6 border-2 border-blue-500">
+        <div class="flex justify-center mb-4">
+            <i class="fas fa-check-circle text-green-500 text-6xl"></i>
+        </div>
+        <h1 class="text-3xl font-bold mb-4 text-center text-blue-700 border-b-2 pb-2">Confirmation</h1>
+        <p class="text-gray-700 text-center text-lg bg-blue-100 p-4 rounded-md">L'email a été envoyé avec succès. Vous serez redirigé dans quelques instants...</p>
+        <p class="text-gray-600 text-center text-lg bg-gray-100 p-4 rounded-md">
+            <i class="fas fa-info-circle text-blue-500 mr-2"></i>
+            Le vendeur prendra contact avec vous prochainement.
+        </p>
     </div>
 </body>
 </html>
--- resources/views/emails/contact_vendeur.blade.php
+++ resources/views/emails/contact_vendeur.blade.php
@@ -4,8 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Interest</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
-<body>
-    <p>{{ $username }} est intéressé par votre produit {{ $productname }}!</p>
+<body class="bg-gray-100 py-8">
+
+<div class="max-w-2xl mx-auto bg-white rounded-lg shadow-md overflow-hidden">
+    <div class="flex justify-center items-center bg-blue-500 p-4">
+        <!-- Logo and title -->
+        <img src="{{ asset('img/fox_icon.png') }}" alt="League of Shop Logo" class="h-10 w-10 mr-2">
+        <span class="text-white font-bold text-xl">League of Shop</span>
+    </div>
+    <div class="p-8">
+        <!-- Message content -->
+        <h1 class="text-xl font-bold mb-4">Notification d'intérêt</h1>
+        <p class="text-gray-700 mb-4">
+            <i class="fas fa-user text-blue-500 mr-2"></i>
+            {{ $username }} est intéressé par votre produit.
+        </p>
+        <p class="text-gray-700 mb-4">
+            <i class="fas fa-box-open text-blue-500 mr-2"></i>
+            Produit : {{ $productname }}
+        </p>
+        <p class="text-gray-700">
+            Merci de continuer à utiliser <strong>League of Shop</strong> pour vos besoins en matière de shopping !
+        </p>
+    </div>
+</div>
+
 </body>
 </html>
